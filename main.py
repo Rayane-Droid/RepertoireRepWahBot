becfe8edf8043b6875e56a122168f2ff2e66cf3d
RAYANE BAKKALI <br.grow.2021@gmail.com>
2025-05-05 19:25:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,22 @@
 import logging
 import os
 
-from keep_alive import keep_alive
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Le bot Telegram est actif."
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
 
 from dotenv import load_dotenv
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
